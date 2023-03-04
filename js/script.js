0edc9fa7589bd1e70d0ea7322acc346c4693d1fc
Butt <121292057+FeBotto@users.noreply.github.com>
2023-03-04 13:21:28 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,3 +20,79 @@
         #se il campo di testo non è vuoto aggiungere il messaggio in coda agli altri del personaggio selezionato
 
 */
+
+function aside(){
+    let lista = `<li>`;
+    let ul = document.getElementById("listUtenti");
+
+    for(let i = 1; i < nomeUtenti.length; i++){
+        if(genereUt[i] == "m"){
+            lista += `<div onclick="header(${i})" class="material-symbols-outlined icone">
+                    face
+                    </div>`;     
+        }
+        else{
+            lista += `<div onclick="header(${i})" class="material-symbols-outlined icone">
+                    face_3
+                    </div>`;
+        }
+        let cognome = cognomeUtenti[i];
+        lista += nomeUtenti[i] + ` ` + cognome[0] + `.`;
+        lista += `</li>`;
+    }
+    ul.innerHTML = lista;
+
+}
+
+
+function header(i){
+    let utente = document.querySelector("header > div:first-child");
+    let nome = document.getElementById("divNome");
+    let ora = document.getElementById("divUltimoMes");
+    
+    if(genereUt[i] == "m"){
+        utente.innerText = "face";
+    }
+    else{
+        utente.innerText = "face_3";
+    }
+
+    nome.innerText = nomeUtenti[i] + " " + cognomeUtenti[i];
+    ora.innerText = "Oggi alle " + ore[i];
+
+    funzmess(i);
+}
+
+function funzmess(numero){
+    let section = document.querySelector("section > section");
+    section.innerText = "";
+
+    for(i in mittenti){
+        if(destinatari[i] == numero){
+            let article = document.createElement("article");
+            article.innerHTML += messaggi[i];
+            article.classList.add("ut1");
+            article.classList.add("mes");
+            section.appendChild(article);
+        }
+        if(mittenti[i] == numero){
+            let article = document.createElement("article");
+            article.innerHTML += messaggi[i];
+            article.classList.add("ut2");
+            article.classList.add("mes");
+            section.appendChild(article);
+        }
+    }
+}
+
+
+function tasto(){
+    let section = document.querySelector("section > section");
+    let article = document.createElement("article");
+    let input = document.querySelector("div > input");
+    article.innerHTML += input.value;
+    article.classList.add("ut1");
+    article.classList.add("mes");
+    section.appendChild(article);
+    input.value = "";
+}
